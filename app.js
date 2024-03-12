4c6cd2276120a54db698e4102e716d126df4acb5
Ira <irawadee.pm@gmail.com>
2024-03-12 13:12:55 +1300

    Finish DOM

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-document.body.children[1].children[0].href = "https://www.google.com/";
+let firstParagraph = document.body.firstElementChild;
 
-// console.dir(document);
+// console.log(firstParagraph);
+
+function changeParagraphText() {
+  firstParagraph.textContent = "Clicked!";
+  console.log(firstParagraph);
+}
+
+firstParagraph.addEventListener("click", changeParagraphText);
+
+// define input
+
+let firstInput = document.body.children[1];
+console.log(firstInput);
+
+function getUserInput(event) {
+  //   let enterText = firstInput.value;
+  let enterText = event.target.value;
+  //   let enterText = event.data;
+  console.log(enterText);
+  //   console.log(event);
+}
+
+firstInput.addEventListener("input", getUserInput);
